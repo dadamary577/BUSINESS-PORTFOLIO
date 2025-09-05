ca95987fad0c22f35c57d238fff544b822c348d7
dadamary577 <dadamary577@gmail.com>
2025-09-05 11:05:02 +0100

    Add files via upload
    UPDATED PORTFOLIO

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // function sendWhatsApp() {
+
 //   let person = document.getElementById("name").value;
 //   let email = document.getElementById("email").value;
 //   let message = document.getElementById("message").value;
@@ -26,32 +27,32 @@
 //   let subject =` Contact from ${person}`;
 //   let body = `person: ${person}%0AEmail: ${email}%0AMessage: ${message}`;
 //   window.location.href = `mailto:dadamary577@gmail.com?subject=${subject}&body=${body}`;
-// }function sendWhatsApp() {
-  let name = document.getElementById("name").value;
-  let email = document.getElementById("email").value;
-  let message = document.getElementById("message").value;
-  let phoneNumber ="07044816854"; // 🔹 Replace with your WhatsApp number
+function sendWhatsApp() {
+  const username = document.getElementById("name").value;
+  const  email = document.getElementById("email").value;
+  const message = document.getElementById("message").value;
+  const phoneNumber ="+2347044816854"; // 🔹 Replace with your WhatsApp number
 
-  if (!names || !email || !message) {
+  if (!username|| !email || !message) {
     alert("Please fill out all fields.");
     return;
   }
-
-  let url = `https://wa.me/${phoneNumber}?text=Hello, my name is ${name}. My email is ${email}. Message: ${message}`;
+  let url = `https://wa.me/${phoneNumber}?text=Hello, my name is ${username}. My email is ${email}. Message: ${message}`;
   window.open(url, "_blank");
+}
 
 
 function sendEmail() {
-  let name = document.getElementById("name").value;
-  let email = document.getElementById("email").value;
-  let message = document.getElementById("message").value;
+  const username = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const message = document.getElementById("message").value;
 
-  if (!name|| !email || !message) {
+  if (!username|| !email || !message) {
     alert("Please fill out all fields.");
     return;
   }
 
-  let subject = `Contact from ${name}`;
-  let body = `Name: ${names}%0AEmail: ${email}%0AMessage: ${message}`;
-  window.location.href = `mailto:yourfarmemail@example.com?subject=${subject}&body=${body}`;
+  let subject = `Contact from ${username}`;
+  let body = `Name: ${username}%0AEmail: ${email}%0AMessage: ${message}`;
+  window.location.href = `mailto:dadamary577@gmail.com?subject=${subject}&body=${body}`;
 }
